70afbf4845ac7535fe639b3d4aa034d14c256ab5
René Pascal Maseli <r.maseli@yahoo.com>
2018-06-08 11:54:24 +0200

    Documented Norms

--- openke/models/norm.py
+++ openke/models/norm.py
@@ -1,11 +1,32 @@
-from tensorflow import reduce_sum as sum, reduce_min as min, reduce_max as max
-
-
 def l1(vectors):
-	'''Implements the l1 norm on a vectorspace.'''
+	'''
+		Implements the l1 norm on a vectorspace.
+
+			Parameters
+		vectors - Tensor of dimension at least one,
+		returning vectors whose norm is to be computed.
+
+			Return Value
+		Tensor of reduced dimension returning the norms.
+		The order is preserved.
+	'''
+
+	from tensorflow import reduce_sum as sum
 	return sum(abs(vectors), -1)
 
 
 def l2(vectors):
-	'''Implements the euclidean norm on a vectorspace.'''
+	'''
+		Implements the euclidean norm on a vectorspace.
+
+			Parameters
+		vectors - Tensor of dimension at least one,
+		returning vectors whose norm is to be computed.
+
+			Return Value
+		Tensor of reduced dimension returning the norms.
+		The order is preserved.
+	'''
+
+	from tensorflow import reduce_sum as sum, sqrt
 	return sqrt(sum(vectors ** 2, -1))
